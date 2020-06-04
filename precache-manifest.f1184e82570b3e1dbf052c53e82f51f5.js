5a4af707f550666e79aab19e42c7023b5d6c61a8
Vladymir10 <adamvladymir@gmail.com>
2020-06-04 10:31:28 -0400

    First commit

---
+++ precache-manifest.f1184e82570b3e1dbf052c53e82f51f5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13da68b55671f980c6042bd815e89972",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eabd1a8b852b8b505d0b",
+    "url": "/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "60dbd0256a93ff63c632",
+    "url": "/static/css/main.f7896e54.chunk.css"
+  },
+  {
+    "revision": "eabd1a8b852b8b505d0b",
+    "url": "/static/js/2.a5747d58.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.a5747d58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60dbd0256a93ff63c632",
+    "url": "/static/js/main.54df0067.chunk.js"
+  },
+  {
+    "revision": "e3f6cca2499cc0ccc243",
+    "url": "/static/js/runtime-main.b2109bab.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
